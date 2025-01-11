6771ec27dc7bde2efc1494e5ceaecffbf8f11e8d
wangyyyqw <1009491591@qq.com>
2025-01-11 21:21:24 +0800

    1

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -24,7 +24,7 @@ export const creators: Creator[] = [
     name: '絢香猫',
     avatar: '',
     username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
+    title: '游侠客 原始创作者',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
     links: [
       { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
@@ -37,7 +37,7 @@ export const creators: Creator[] = [
     name: '絢香音',
     avatar: '',
     username: 'LittleSound',
-    title: 'Nólëbase 原始创作者',
+    title: '游侠客 原始创作者',
     desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
     links: [
       { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
@@ -45,7 +45,21 @@ export const creators: Creator[] = [
     ],
     nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
     emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
-  },*/
+  },
+    {
+    name: '未响应',
+    avatar: '',
+    username: '',
+    title: 'epub文件加密软件提供者',
+    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+    links: [
+      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+    ],
+    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
+    emailAliases: ['neko@ayaka.moe'],
+  },
+  */
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
   return c as Creator
--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = '游侠客'
+export const siteShortName = '游侠客'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
